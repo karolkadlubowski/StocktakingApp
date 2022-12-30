bbabc70709487fcd28bdc8a51f1b9950a86418a1
karolkadlubowski <karol.kadlubowski@infotower.pl>
2022-12-30 01:57:25 +0100

    [12] Tests config

---
+++ app/src/test/java/pl/polsl/stocktakingApp/base/CoroutinesTestRule.kt
@@ -0,0 +1,22 @@
+package pl.polsl.stocktakingApp.base
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CoroutinesTestRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : BeforeEachCallback, AfterEachCallback, CoroutineScope by TestScope(testDispatcher) {
+    override fun beforeEach(context: ExtensionContext?) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        Dispatchers.resetMain()
+    }
+}
---
+++ app/src/test/java/pl/polsl/stocktakingApp/screen/ListScreenViewModelTest.kt
@@ -0,0 +1,4 @@
+package pl.polsl.stocktakingApp.screen
+
+class ListScreenViewModelTest {
+}
